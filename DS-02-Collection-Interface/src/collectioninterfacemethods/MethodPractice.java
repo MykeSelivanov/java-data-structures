323fe35f27080dcb09cbe9cb1afaf8b1e268bc09
MykeSelivanov <michael777selivanov@gmail.com>
2020-11-21 22:28:36 -0600

    Collection interface

--- DS-02-Collection-Interface/src/collectioninterfacemethods/MethodPractice.java
+++ DS-02-Collection-Interface/src/collectioninterfacemethods/MethodPractice.java
@@ -19,7 +19,7 @@ public class MethodPractice {
         System.out.println("coll.add(10) : " + coll.add(10));
 
         // adding multiple items in one shot
-        coll.addAll(Arrays.asList(1,2,3,4,5,6,7,8,9,10));
+        coll.addAll(Arrays.asList(1,2,3,4,5,6,7,9));
         System.out.println(coll);
 
         // getting size
@@ -31,6 +31,23 @@ public class MethodPractice {
         // remove item
         coll.remove(7);
         System.out.println(coll);
+        // remove method returns boolean
+        System.out.println("coll.remove(17) : " + coll.remove(17));
+
+        // contains item
+        System.out.println("coll.contains(7) : " + coll.contains(7));
+
+        // contains All items
+        System.out.println("coll.containsAll(Arrays.asList(30, 5, 9)) : " + coll.containsAll(Arrays.asList(30, 5, 9)));
+
+        // remove All
+        coll.removeAll(Arrays.asList(3, 5, 10));
+        System.out.println(coll);
+        // remove all returns boolean
+        System.out.println("coll.removeAll(Arrays.asList(30, 2, 4)) : " + coll.removeAll(Arrays.asList(30, 2, 4)));
+
+        // retains All
+
 
 
     }
