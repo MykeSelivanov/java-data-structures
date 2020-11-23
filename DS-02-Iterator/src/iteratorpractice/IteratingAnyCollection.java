ecfd6aeb0496ddb0ce30db50f5076ce885558a4a
MykeSelivanov <michael777selivanov@gmail.com>
2020-11-22 22:18:59 -0600

    Iterator

--- DS-02-Iterator/src/iteratorpractice/IteratingAnyCollection.java
+++ DS-02-Iterator/src/iteratorpractice/IteratingAnyCollection.java
@@ -18,7 +18,7 @@ public class IteratingAnyCollection {
          // hasNext(); return boolean, does not move iterator to the next location
         System.out.println(myIterator.hasNext());
 
-        // next(); moves iterator to the next location
+        // next(); moves iterator to the next location, whenever you call this method
         System.out.println(myIterator.next()); // 1
         System.out.println(myIterator.next()); // 2
         System.out.println(myIterator.next()); // 3
@@ -27,7 +27,15 @@ public class IteratingAnyCollection {
         myIterator.remove(); //3 was removed
         System.out.println(coll);
 
+        Collection<Integer> numbers = new ArrayList<>(Arrays.asList(10,11,12,13,14,15,16,17,18,19,20));
+        System.out.println(numbers);
 
+        Iterator<Integer> numsIterator = numbers.iterator();
+
+        // remove all numbers > 15
+        while (numsIterator.hasNext()) {
+            Integer number =
+        }
 
 
 
