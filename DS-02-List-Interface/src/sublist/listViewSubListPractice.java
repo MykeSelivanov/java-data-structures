a396278167768a1858b16c5f10d1de55f62e73fd
MykeSelivanov <michael777selivanov@gmail.com>
2020-11-24 22:48:35 -0600

    List View - Sublist

---
+++ DS-02-List-Interface/src/sublist/listViewSubListPractice.java
@@ -0,0 +1,14 @@
+package sublist;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+public class listViewSubListPractice {
+    public static void main(String[] args) {
+
+        List<Integer> list = new LinkedList<>(Arrays.asList(1,2,3,4,5,6,7,8,9));
+        System.out.println(list);
+
+    }
+}
