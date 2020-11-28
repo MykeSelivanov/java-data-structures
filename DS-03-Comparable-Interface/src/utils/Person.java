98d279a4fdce8651ad9792c3bb7f957c72f2258e
MykeSelivanov <michael777selivanov@gmail.com>
2020-11-27 20:09:05 -0600

    Comparable Interface

--- DS-03-Comparable-Interface/src/utils/Person.java
+++ DS-03-Comparable-Interface/src/utils/Person.java
@@ -18,10 +18,16 @@ public class Person implements Comparable<Person> {
     @Override
     public int compareTo(Person o) {
 
-        if(this.name.charAt(0) > o.name.charAt(0)) {
+        if (this.name.charAt(0) > o.name.charAt(0)) {
             return 1;
         } else if (this.name.charAt(0) == o.name.charAt(0)) {
-            return 0;
+            if (this.age > o.age) {
+                return 1;
+            } else if (this.age == o.age) {
+                return 0;
+            } else {
+                return -1;
+            }
         } else {
             return -1;
         }
