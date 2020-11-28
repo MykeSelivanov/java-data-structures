8da37c6eab85b2b6ac84ea0095d3a73a7d1a3fa3
MykeSelivanov <michael777selivanov@gmail.com>
2020-11-27 19:44:00 -0600

    Comparable Interface

---
+++ DS-03-Comparable-Interface/src/practice/CompatingObjects.java
@@ -0,0 +1,21 @@
+package practice;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class CompatingObjects {
+    public static void main(String[] args) {
+
+        List<String> list = new ArrayList<>();
+        list.addAll(Arrays.asList("Mike","Stacy","Peter","Diana"));
+
+        System.out.println(list);
+        Collections.sort(list);
+        System.out.println(list);
+
+
+    }
+
+}
