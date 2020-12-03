48e6684f16ae0e6177a0be7e5f2f6d40bbd57be8
MykeSelivanov <michael777selivanov@gmail.com>
2020-12-02 19:32:05 -0600

    Custom Binary Search Tree implementation

--- DS-09-Trees/src/utils/TreeClass.java
+++ DS-09-Trees/src/utils/TreeClass.java
@@ -24,8 +24,27 @@ public class TreeClass {
         // if tree is not empty
         Node current = root;
         while (true) {
-
+            // check if less
+            if(value < current.value) {
+                if (current.leftChild == null) {
+                    current.leftChild = node;
+                    break;
+                } else {
+                    current = current.leftChild;
+                }
+            } else {
+                if (current.rightChild == null) {
+                    current.rightChild = node;
+                    break;
+                } else {
+                    current = current.rightChild;
+                }
+            }
         }
+    }
+    // Preorder Root-Left-right
+    private void traversePreOrder(Node root){
+
     }
 
     public boolean isEmpty(){
