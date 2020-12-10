25031f8becac9f676f7c1bbedefd8026d5d2fed3
MykeSelivanov <michael777selivanov@gmail.com>
2020-12-09 18:46:24 -0600

    Custom Heap

--- DS-09-Heap/src/utils/MyHeap.java
+++ DS-09-Heap/src/utils/MyHeap.java
@@ -3,6 +3,15 @@ package utils;
 public class MyHeap {
 
     private final int [] heap = new int[20];
+    private int size;
+
+    private int parent(int index){
+        return (index - 1) / 2;
+    }
+
+    public boolean isFull(){
+        return size == heap.length;
+    }
 
 
 }
