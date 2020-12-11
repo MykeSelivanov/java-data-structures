0590a35745d53a1c800672c3c266dd8e31617cd0
MykeSelivanov <michael777selivanov@gmail.com>
2020-12-10 23:03:50 -0600

    Weighted Graph

--- DS-10-Graphs/src/utils/WeightedGraph.java
+++ DS-10-Graphs/src/utils/WeightedGraph.java
@@ -22,6 +22,26 @@ public class WeightedGraph {
 
     public void addEdge(int source, int target, int w) { edges[source][target] = w; }
     public boolean isEdge(int source, int target) { return edges[source][target] > 0; }
-    public void removeEdge(int source, int target)
+    public void removeEdge(int source, int target) { edges[source][target] = 0; }
+    public int     getWeight  (int source, int target)  { return edges[source][target]; }
+
+    public static int getByLabel(Object theLabel){
+        int vertexNum=0;
+        for (int i=0;i<labels.length;i++)
+            if (labels[i]==theLabel) vertexNum=i;
+        return vertexNum;
+    }
+    public int [] neighbors (int vertex) {
+        int count = 0;
+        for (int i=0; i < edges[vertex].length; i++) {
+            if (edges[vertex][i] > 0) count++;
+        }
+        final int[] answer= new int[count];
+        count = 0;
+        for (int i=0; i < edges[vertex].length; i++) {
+            if (edges[vertex][i] > 0) answer[count++] = i;
+        }
+        return answer;
+    }
 
 }
