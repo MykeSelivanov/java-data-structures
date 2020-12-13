8d2001c9b97d8eac921b8cb3380c2e590dbfdf2d
MykeSelivanov <michael777selivanov@gmail.com>
2020-12-13 15:04:08 -0600

    HashSet

--- DS-07-HashSet/src/hashsetpractice/SetOfString_HashSet.java
+++ DS-07-HashSet/src/hashsetpractice/SetOfString_HashSet.java
@@ -7,10 +7,19 @@ public class SetOfString_HashSet {
     public static void main(String[] args) {
 
         System.out.println(new String("Adam").hashCode());
-        System.out.println(new String("Adam").hashCode()); // same hashcode value as above
+        System.out.println(new String("John").hashCode()); // same hashcode value as above
         // hashcode is equal therefore .equal method will return true
 
         Set<String> stringSet = new HashSet<>();
+        stringSet.add("Adam");
+        stringSet.add("John");
+        /*
+        HashCode    |   Value
+        2035631     |   Adam
+        2314539     |   John
+         */
+
+        System.out.println(stringSet.size());
 
     }
 
