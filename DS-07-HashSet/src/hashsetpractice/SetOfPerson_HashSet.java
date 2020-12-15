0adb14436d0e682dfe33bd5c51cfeda23cea1298
MykeSelivanov <michael777selivanov@gmail.com>
2020-12-14 21:02:42 -0600

    HashSet

--- DS-07-HashSet/src/hashsetpractice/SetOfPerson_HashSet.java
+++ DS-07-HashSet/src/hashsetpractice/SetOfPerson_HashSet.java
@@ -1,7 +1,22 @@
 package hashsetpractice;
 
-public class SetOfPerson_HashSet {
+import utils.Person;
 
+import java.util.HashSet;
+import java.util.Set;
+
+public class SetOfPerson_HashSet {
+    public static void main(String[] args) {
+
+        Set<Person> personSet = new HashSet<>();
+
+        Person p1 = new Person("John",20);
+        Person p2 = new Person("John",20);
+        Person p3 = new Person("John",20);
+
+        System.out.println();
+
+    }
 
 
 }
