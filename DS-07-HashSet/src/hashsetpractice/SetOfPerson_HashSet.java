581f6ca181f6d1f9e8ae3e84e6b6399820abf09d
MykeSelivanov <michael777selivanov@gmail.com>
2020-12-14 21:57:47 -0600

    HashSet

--- DS-07-HashSet/src/hashsetpractice/SetOfPerson_HashSet.java
+++ DS-07-HashSet/src/hashsetpractice/SetOfPerson_HashSet.java
@@ -14,7 +14,17 @@ public class SetOfPerson_HashSet {
         Person p2 = new Person("John",20);
         Person p3 = new Person("John",20);
 
-        System.out.println();
+        System.out.println(p1.hashCode());
+        System.out.println(p2.hashCode());
+        System.out.println(p3.hashCode());
+
+        // each hashcode will be unique, meaning all of those objects will be added to HashSet
+        personSet.add(p1);
+        personSet.add(p2);
+        personSet.add(p3);
+
+        System.out.println(personSet.size());
+
 
     }
 
