d2aba4a6e93c761d6e79be7fb8ed4debb4d16b90
MykeSelivanov <michael777selivanov@gmail.com>
2020-12-17 19:58:21 -0600

    HashSet

--- DS-07-HashSet/src/utils/Person.java
+++ DS-07-HashSet/src/utils/Person.java
@@ -1,5 +1,7 @@
 package utils;
 
+import java.util.Objects;
+
 public class Person {
 
     String name;
@@ -30,6 +32,17 @@ public class Person {
 //        return false;
 //    }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return age == person.age &&
+                Objects.equals(name, person.name);
+    }
 
-
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age);
+    }
 }
