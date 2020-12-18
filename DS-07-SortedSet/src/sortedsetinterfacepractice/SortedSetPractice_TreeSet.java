cfc158e4409a539942ffd6258b0887608e50c428
MykeSelivanov <michael777selivanov@gmail.com>
2020-12-18 17:58:23 -0600

    SortedSet

--- DS-07-SortedSet/src/sortedsetinterfacepractice/SortedSetPractice_TreeSet.java
+++ DS-07-SortedSet/src/sortedsetinterfacepractice/SortedSetPractice_TreeSet.java
@@ -9,7 +9,11 @@ public class SortedSetPractice_TreeSet {
         // SortedSet is a type of set that maintain Sorted Order on top of uniqueness
 
         SortedSet<Long> longTreeSet = new TreeSet<>();
-        longTreeSet.add(3L);
+        System.out.println("longTreeSet.add(3L): " + longTreeSet.add(3L));
+        System.out.println("longTreeSet.add(20L): " + longTreeSet.add(20L));
+        System.out.println("longTreeSet.add(39L): " + longTreeSet.add(39L));
+        System.out.println("longTreeSet.add(27L): " + longTreeSet.add(27L));
+        System.out.println("longTreeSet.add(20L): " + longTreeSet.add(20L));
         longTreeSet.add(3L);
         longTreeSet.add(39L);
         longTreeSet.add(30L);
