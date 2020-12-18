399732653c2433a0bdd09351a3ee74f51491e9ac
MykeSelivanov <michael777selivanov@gmail.com>
2020-12-18 17:53:56 -0600

    SortedSet

---
+++ DS-07-SortedSet/src/sortedsetinterfacepractice/SortedSetPractice_TreeSet.java
@@ -0,0 +1,13 @@
+package sortedsetinterfacepractice;
+
+public class SortedSetPractice_TreeSet {
+    public static void main(String[] args) {
+
+        // SortedSet is a type of set that maintain Sorted Order on top of uniqueness
+
+
+
+
+    }
+
+}
