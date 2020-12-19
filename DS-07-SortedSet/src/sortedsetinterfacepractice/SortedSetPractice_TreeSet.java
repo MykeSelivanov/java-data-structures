fde360b3279a822660b009d6d36a2704ac32526c
MykeSelivanov <michael777selivanov@gmail.com>
2020-12-18 18:08:25 -0600

    SortedSet

--- DS-07-SortedSet/src/sortedsetinterfacepractice/SortedSetPractice_TreeSet.java
+++ DS-07-SortedSet/src/sortedsetinterfacepractice/SortedSetPractice_TreeSet.java
@@ -1,5 +1,6 @@
 package sortedsetinterfacepractice;
 
+import java.util.Arrays;
 import java.util.SortedSet;
 import java.util.TreeSet;
 
@@ -19,6 +20,26 @@ public class SortedSetPractice_TreeSet {
 
         System.out.println("longTreeSet.first(): " + longTreeSet.first());
         System.out.println("longTreeSet.last(): " + longTreeSet.last());
+
+        longTreeSet.addAll(Arrays.asList(15L, 18L, 15L, 18L));
+        System.out.println(longTreeSet);
+
+        // SortedSet -->> null is not allowed!
+        // longTreeSet.add(null);
+
+        // Getting the view out of SortedSet
+        // SortedSet<E> subSet(E fromElement, E toElement);
+        // SortedSet<E> headSet(E toElement);
+        // SortedSet<E> tailSet(E fromElement);
+
+        System.out.println("longTreeSet.subSet(15L, 21L): " + longTreeSet.subSet(15L, 21L));
+        System.out.println("longTreeSet.headSet(18L): " + longTreeSet.headSet(18L));
+        System.out.println("longTreeSet.tailSet(18L): " + longTreeSet.tailSet(18L));
+
+        SortedSet<Long> tailView = longTreeSet.tailSet(18L);
+        tailView.remove(20L); // removing from a view will also remove from an original set, view is a part of the original list
+        System.out.println(tailView);
+        System.out.println(longTreeSet);
 
 
     }
