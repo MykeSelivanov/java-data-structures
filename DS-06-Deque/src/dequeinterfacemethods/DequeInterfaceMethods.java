5f1b39b27f88551667447760a4bd4197565a340d
MykeSelivanov <michael777selivanov@gmail.com>
2020-12-21 22:40:55 -0600

    Deque Interface methods

---
+++ DS-06-Deque/src/dequeinterfacemethods/DequeInterfaceMethods.java
@@ -0,0 +1,29 @@
+package dequeinterfacemethods;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class DequeInterfaceMethods {
+    public static void main(String[] args) {
+
+        /**
+         * Deque methods
+         *
+         *           Head        |        Tail
+         *
+         * Insert:  addFirst(e)  |   addLast(e)
+         *          offerFirst(e)|   offerLast(e)
+         *          push(e)      |
+         * Remove:  removeFirst()|   removeLast()
+         *          pollFirst    |   pollLast()
+         *          pop()        |
+         * Examine: getFirst()   |   getLast()
+         *          peekFirst()  |   peekLast()
+         */
+
+        Deque<Integer> dQue = new ArrayDeque<>();
+
+
+    }
+
+}
