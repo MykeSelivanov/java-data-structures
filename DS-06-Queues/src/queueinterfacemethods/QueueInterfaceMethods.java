908987cbea493aae2e37c78f768917381b5d8288
MykeSelivanov <michael777selivanov@gmail.com>
2020-12-21 22:22:38 -0600

    Queue Interface methods

---
+++ DS-06-Queues/src/queueinterfacemethods/QueueInterfaceMethods.java
@@ -0,0 +1,27 @@
+package queueinterfacemethods;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class QueueInterfaceMethods {
+    public static void main(String[] args) {
+
+        /**
+         * Queue methods
+         *
+         * Queue can be implemented with LinkedList or ArrayDeque
+         *
+         *          Throws exception     |   Returns Special value
+         * Insert:  add(e)               |  offer(e)
+         * Remove:  remove()             | poll()
+         * Examine: element()            | peek()
+         */
+
+        Queue<Integer> numQue = new LinkedList<>();
+
+
+
+    }
+
+
+}
