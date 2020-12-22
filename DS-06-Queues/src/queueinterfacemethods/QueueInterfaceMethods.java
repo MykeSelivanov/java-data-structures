e9322f0469529702ab56f4ab30805aab41010c84
MykeSelivanov <michael777selivanov@gmail.com>
2020-12-21 22:26:13 -0600

    Queue Interface methods

--- DS-06-Queues/src/queueinterfacemethods/QueueInterfaceMethods.java
+++ DS-06-Queues/src/queueinterfacemethods/QueueInterfaceMethods.java
@@ -1,5 +1,6 @@
 package queueinterfacemethods;
 
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 
@@ -18,6 +19,16 @@ public class QueueInterfaceMethods {
          */
 
         Queue<Integer> numQue = new LinkedList<>();
+        numQue.offer(1);
+        numQue.offer(3);
+        numQue.offer(4);
+        numQue.offer(6);
+        numQue.offer(8);
+        numQue.addAll(Arrays.asList(4,9,66));
+
+        System.out.println(numQue);
+        System.out.println(numQue.remove());
+        System.out.println(numQue.poll()); // safer way to remove, avoiding risk to run into exception
 
 
 
