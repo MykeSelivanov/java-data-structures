a6ff3c2c0d1151cc5aef43356ed7ff032961f7ae
MykeSelivanov <michael777selivanov@gmail.com>
2020-12-23 15:52:38 -0600

    Map

---
+++ DS-08-Map/src/mapinterfacepractice/MapInterfacePractice.java
@@ -0,0 +1,13 @@
+package mapinterfacepractice;
+
+public class MapInterfacePractice {
+    public static void main(String[] args) {
+        /**
+         * An object that maps keys to values
+         * A map cannot contain duplicate keys
+         *
+         */
+
+    }
+
+}
