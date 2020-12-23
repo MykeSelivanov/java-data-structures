cac2476df1a5532c6d77776b253b7ba6621b787d
MykeSelivanov <michael777selivanov@gmail.com>
2020-12-23 15:58:21 -0600

    Map

--- DS-08-Map/src/mapinterfacepractice/MapInterfacePractice.java
+++ DS-08-Map/src/mapinterfacepractice/MapInterfacePractice.java
@@ -17,6 +17,17 @@ public class MapInterfacePractice {
          *
          */
 
+        //-----Map Interface Methods-----
+        // int size();
+        // boolean isEmpty();
+        // boolean containsKey(Object key);
+        // boolean containsValue(Object value);
+        // V get(Object key);
+        // V put(K key, V value);
+        // V remove(Object key);
+        // void putAll(Map<? extends K, ? extends V> m);
+        // void clear();
+
 
 
     }
