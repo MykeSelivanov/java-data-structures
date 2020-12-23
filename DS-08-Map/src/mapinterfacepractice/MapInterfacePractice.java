7c043c54a6411df1e4d03c14f1f35260ba1a269a
MykeSelivanov <michael777selivanov@gmail.com>
2020-12-23 15:55:07 -0600

    Map

--- DS-08-Map/src/mapinterfacepractice/MapInterfacePractice.java
+++ DS-08-Map/src/mapinterfacepractice/MapInterfacePractice.java
@@ -4,9 +4,20 @@ public class MapInterfacePractice {
     public static void main(String[] args) {
         /**
          * An object that maps keys to values
-         * A map cannot contain duplicate keys
+         * A map cannot contain duplicate keys, each key can map to at most one value
+         *
+         *      Map
+         *   General implementation - HashMap
+         *
+         *   SortedMap extends Map interface
+         *   NavigatableMap extends SortedMap interface
+         *   TreeMap implements NavigatableMap
+         *
+         *   Legacy class HashTable
          *
          */
+
+
 
     }
 
