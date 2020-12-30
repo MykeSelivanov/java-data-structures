8ce90b62d5e30ff3f2050fa381afb7f0f5cc4ad8
MykeSelivanov <michael777selivanov@gmail.com>
2020-12-29 20:21:45 -0600

    Merge sort

---
+++ DS-11-Sorting/src/bubblesort/MyMergeSort.java
@@ -0,0 +1,52 @@
+package bubblesort;
+
+import java.util.Arrays;
+
+public class MyMergeSort {
+    public void mergeSort(int [] array){
+        if(array.length < 2) return; // termination criteria
+
+        int middle = array.length / 2; // calculating middle value
+
+        int[] leftSubArray = new int[middle]; // new subArray for the left part
+        for( int i = 0; i < middle; i++){ // assign values from start to middle of original array to the left part subArray
+            leftSubArray[i] = array[i];
+        }
+
+        int[] rightSubArray = new int[array.length - middle]; // new subArray for the right part
+        for(int i = middle; i < array.length; i++){ // assign values from middle to end of original array to the right part subArray
+            rightSubArray[i - middle] = array[i];
+        }
+
+        mergeSort(leftSubArray); // call mergeSort recursively for the left subArray
+        mergeSort(rightSubArray); // call mergeSort recursively for the right subArray
+        merge(leftSubArray, rightSubArray, array); // merge solutions into result array
+    }
+
+    private void merge(int[] leftSubArray, int[] rightSubArray, int[] resultArray){
+        int i = 0, j = 0, k = 0;
+        while (i < leftSubArray.length && j < rightSubArray.length){
+            if (leftSubArray[i] <= rightSubArray[j]){
+                resultArray[k] = leftSubArray[i];
+                k++;
+                i++;
+            } else {
+                resultArray[k] = rightSubArray[j];
+                k++;
+                j++;
+            }
+        }
+        while (i < leftSubArray.length){
+            resultArray[k] = leftSubArray[i];
+            k++;
+            i++;
+        }
+        while (j < rightSubArray.length){
+            resultArray[k] = rightSubArray[j];
+            k++;
+            j++;
+        }
+
+    }
+
+}
--- DS-11-Sorting/src/runner/SoringMainApp.java
+++ DS-11-Sorting/src/runner/SoringMainApp.java
@@ -2,6 +2,7 @@ package runner;
 
 import bubblesort.MyBubbleSort;
 import bubblesort.MyInsertionSort;
+import bubblesort.MyMergeSort;
 import bubblesort.MySelectionSort;
 
 import java.util.Arrays;
@@ -9,20 +10,32 @@ import java.util.Arrays;
 public class SoringMainApp {
     public static void main(String[] args) {
 
-        var myArray = new int[] {8,2,4,1,3};
+        var myArray = new int[] {8,2,4,1,3,-1,0};
 
         // Bubble sort
+        myArray = new int[]{8,2,4,1,3,-1,0};
+        System.out.println("Bubble sort for: " + Arrays.toString(myArray));
         var myBubbleSort = new MyBubbleSort();
         System.out.println(Arrays.toString(myBubbleSort.bubbleSort(myArray)));
 
         // Selection sort
+        myArray = new int[]{8,2,4,1,3,-1,0};
+        System.out.println("Selection sort for: " + Arrays.toString(myArray));
         var mySelectionSort = new MySelectionSort();
         System.out.println(Arrays.toString(mySelectionSort.selectionSort(myArray)));
 
         // Insertion sort
+        myArray = new int[]{8,2,4,1,3,-1,0};
+        System.out.println("Insertion sort for: " + Arrays.toString(myArray));
         var myInsertionSort = new MyInsertionSort();
         System.out.println(Arrays.toString(myInsertionSort.insertionSort(myArray)));
 
+        // Merge sort
+        myArray = new int[]{8,2,4,1,3,-1,0};
+        System.out.println("Merge sort for: " + Arrays.toString(myArray));
+        var myMergeSort = new MyMergeSort();
+        myMergeSort.mergeSort(myArray);
+        System.out.println(Arrays.toString(myArray));
 
 
     }
