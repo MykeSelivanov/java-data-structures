4c99785eb2b7ae7e5c201626b2b02eac01d1ad6d
MykeSelivanov <michael777selivanov@gmail.com>
2020-12-30 19:08:37 -0600

    Ternary search

--- DS-12-Searching/src/runner/SearchMainApp.java
+++ DS-12-Searching/src/runner/SearchMainApp.java
@@ -3,6 +3,7 @@ package runner;
 import searchingalgorithms.MyBinarySearchIterative;
 import searchingalgorithms.MyBinarySearchRecursive;
 import searchingalgorithms.MyLinearSearch;
+import searchingalgorithms.MyTernarySearch;
 
 public class SearchMainApp {
     public static void main(String[] args) {
@@ -16,12 +17,15 @@ public class SearchMainApp {
         // List should be always sorted before applying Binary search!!! typically quick sort is used before searching
         // Binary search recursive
         var myBinarySearchRecursive = new MyBinarySearchRecursive();
-        System.out.println("Binary search recursive: " + myBinarySearchRecursive.binarySearchRecursive(myArray, 112));
+        System.out.println("Binary search recursive: " + myBinarySearchRecursive.binarySearchRecursive(myArray, 3));
 
         // Binary search iterative
         var myBinarySearchIterative = new MyBinarySearchIterative();
-        System.out.println("Binary search iterative: " + myBinarySearchIterative.binarySearchIterative(myArray, 112));
+        System.out.println("Binary search iterative: " + myBinarySearchIterative.binarySearchIterative(myArray, 89));
 
+        // Ternary search
+        var myTernarySearch = new MyTernarySearch();
+        System.out.println("Ternary search: " + myTernarySearch.ternarySearch(myArray, 5));
 
     }
 
