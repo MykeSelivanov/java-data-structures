a7f179baa32314297a8c89afe8de115ea2b4168d
MykeSelivanov <michael777selivanov@gmail.com>
2021-01-03 20:54:46 -0600

    Map

--- DS-08-Map/src/mapinterfacepractice/MapView.java
+++ DS-08-Map/src/mapinterfacepractice/MapView.java
@@ -23,11 +23,22 @@ public class MapView {
         keys.remove("Adam"); // after removing from keyView it will also reflect on the original map
         System.out.println(scoreMap);
 
+        for (String key: keys) {
+            System.out.println("Key is: " + key + " value is: " + scoreMap.get(key));
+        }
+
         // Collection<V> values();
         Collection<Integer> valuesList = scoreMap.values();
         System.out.println(valuesList);
         valuesList.remove(55); // removing from a valueView will also reflect on the original map
         System.out.println(scoreMap);
+
+        // Set<Entry<K,V>> entrySet(); - Entry is an interface embedded into map interface and it is used to represent
+        // mapEntry - key,value pair together as one
+        // when you get the entrySet view out of the map your datatype will be the Entry, each item in the Set will be
+        // seen as one entry, there are 3 methods available in entry interface:
+        // getValue(), setValue(), getKey()
+
 
 
     }
