56c331e5d6c27fa0f2aa1369a659667c316a2783
MykeSelivanov <michael777selivanov@gmail.com>
2021-01-03 20:43:56 -0600

    Map

--- DS-08-Map/src/mapinterfacepractice/MapView.java
+++ DS-08-Map/src/mapinterfacepractice/MapView.java
@@ -2,6 +2,7 @@ package mapinterfacepractice;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 public class MapView {
     public static void main(String[] args) {
@@ -9,14 +10,19 @@ public class MapView {
         Map<String,Integer> scoreMap = new HashMap<>();
 
         scoreMap.put("Adam",90);
-        scoreMap.put("Adam",90);
-        scoreMap.put("Adam",90);
-        scoreMap.put("Adam",90);
-        scoreMap.put("Adam",90);
+        scoreMap.put("John",90);
+        scoreMap.put("Mary",90);
+        scoreMap.put("Pete",90);
+        scoreMap.put("Glen",90);
 
         System.out.println(scoreMap);
 
         //--------------- Map View ---------------
+
+        // Set<K> keySet();
+        Set<String> keys = scoreMap.keySet();
+        System.out.println(keys);
+
 
     }
 
