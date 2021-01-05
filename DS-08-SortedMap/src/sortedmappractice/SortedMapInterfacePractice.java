4b48a6cbfb2e911035f51a96aba7fc753f23e7c1
MykeSelivanov <michael777selivanov@gmail.com>
2021-01-04 21:18:39 -0600

    Map

---
+++ DS-08-SortedMap/src/sortedmappractice/SortedMapInterfacePractice.java
@@ -0,0 +1,22 @@
+package sortedmappractice;
+
+import java.util.SortedMap;
+import java.util.TreeMap;
+
+public class SortedMapInterfacePractice {
+    public static void main(String[] args) {
+
+        /** Map
+         * General implementation -- HashMap
+         *
+         * SortedMap      extends Map interface
+         * NavigatableMap extends SortedMap interface
+         * TreeMap        implements NavigatableMap
+         */
+
+        SortedMap<String,Integer> scoreSortedMap = new TreeMap<>();
+
+
+    }
+
+}
